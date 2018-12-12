4c1fcf4d70bef523e896df54bd340b7754d2336e
Taylor Alexandra Hutchison <aibhleog@users.noreply.github.com>
2018-12-11 14:34:52 -1000

    de-redshift code
    given a redshift and an observed wavelength, gives the de-redshifted wavelength.
    This is helpful if you know the redshift of your source, and you see an interesting feature and you want to know the rest-wavelength for it.
    
    (disclaimer -- I don't use this very often but it's nice to have)

---
+++ de.redshift.py
@@ -0,0 +1,15 @@
+'''
+This script calculates the wavelength of a line (in Angstroms) given its
+restframe wavelength (in Angstroms) and a redshift.
+'''
+
+import numpy as np
+# returns the redshifted line
+def de_redshift(lam,z):
+	print(r'De-redshifted line: %s angstroms'%(lam / (z + 1)))
+	print()
+
+# reads in input when scripting
+if __name__ == "__main__":
+	import sys
+	de_redshift(float(sys.argv[1]),float(sys.argv[2]))
