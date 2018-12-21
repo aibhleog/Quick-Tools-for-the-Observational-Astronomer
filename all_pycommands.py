a3ce460995fbf280c14587b705ecf9dffc865b0e
Taylor Alexandra Hutchison <aibhleog@users.noreply.github.com>
2018-12-21 10:45:03 -0600

    deleting aliases I don't provide here

--- all_pycommands.py
+++ all_pycommands.py
@@ -7,11 +7,6 @@ redshift -- calculates the redshift given a restframe & observed line
 zlam -- redshifts a line given restframe wavelength and redshift
 dered -- returns the rest wavelength given an observed line and redshift
 
-zlines -- shows a redshifted spectrum with relevant lines & NIR/IR filters
-dex -- shows dex.png
-fullage -- shows a figure with the age of the Universe afo redshift
-zage -- given a redshift will return the age of the Universe & other cosmology
-
 sep -- returns the separation between two coordinates of same units
 sepmix -- returns the separation between two coordinates of different units
 coord -- converts given coordinates to a specified format
