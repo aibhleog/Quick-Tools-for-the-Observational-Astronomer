807db981e3de761169fbe9604a4c673d20cae134
Taylor A. Hutchison <aibhleog@users.noreply.github.com>
2024-08-30 20:10:02 -0400

    updating what prints :fire:

--- all_pycommands.py
+++ all_pycommands.py
@@ -6,6 +6,7 @@ offset -- calculates the velocity offset between two redshifts
 redshift -- calculates the redshift given a restframe & observed line
 zlam -- redshifts a line given restframe wavelength and redshift
 dered -- returns the rest wavelength given an observed line and redshift
+zlines -- pops up a plot that shows a model spectrum redshifted with bandpasses overlaid
 
 sep -- returns the separation between two coordinates of same units
 sepmix -- returns the separation between two coordinates of different units
